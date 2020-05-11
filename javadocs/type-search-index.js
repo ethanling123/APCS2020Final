c25329c78f6fcc099d0c113835911be7f6fe78fc
Ethan <ethanling123@gmail.com>
2020-05-10 19:22:09 -0700

    Javadocs and jar
    Plus a few comments

--- javadocs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"p":"<Unnamed>","l":"Actor"},{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"Asteroid"},{"p":"<Unnamed>","l":"DamagableInterface"},{"p":"<Unnamed>","l":"GameCharacter"},{"p":"<Unnamed>","l":"Main"},{"p":"<Unnamed>","l":"MovingImage"},{"p":"<Unnamed>","l":"PlayerCharacter"},{"p":"<Unnamed>","l":"Projectile"}]
